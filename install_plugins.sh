82d7c972e7da5003bd507994300257a543d00185
Mar Koss <mar.koss@icloud.com>
2021-11-06 13:45:12 -0700

    added .gitconfig and global gitignore

---
+++ install_plugins.sh
@@ -0,0 +1,30 @@
+
+# vim config directory
+if [ ! -d "$HOME/.vim" ]; then
+    mkdir ~/.vim
+fi
+
+# the vim autoload directory
+if [ ! -d "$HOME/.vim/autoload" ]; then
+    mkdir -p ~/.vim/autoload 
+fi
+
+# the vim/pathogen bundle directory
+if [ ! -d "$HOME/.vim/bundle" ]; then
+    mkdir -p ~/.vim/bundle 
+fi
+
+# the vim/pathogen bundle directory
+if [ ! -d "$HOME/.vim/colors" ]; then
+    mkdir -p ~/.vim/colors 
+fi
+
+# install pathogen
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# install the bundles
+pushd ~/.vim/bundle
+
+# install NERDCommenter
+cd ~/.vim/bundle
+git clone https://github.com/preservim/nerdcommenter.git
