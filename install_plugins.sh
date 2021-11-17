160ab793e0f45d9e24c64d7065aaf3689e73e3e1
Mar Koss <mar.koss@icloud.com>
2021-11-17 14:04:42 -0800

    added zshrc dotfile

--- install_plugins.sh
+++ install_plugins.sh
@@ -28,3 +28,6 @@ pushd ~/.vim/bundle
 # install NERDCommenter
 cd ~/.vim/bundle
 git clone https://github.com/preservim/nerdcommenter.git
+
+# Install plugin directory
+mkdir -p ~/.vim/plugin && curl -LSso ~/.vim/plugin/taboo.vim https://raw.githubusercontent.com/gcmt/taboo.vim/master/plugin/taboo.vim
